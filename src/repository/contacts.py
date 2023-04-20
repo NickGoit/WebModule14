6b5efc80444f7b0c126752b86fcb8acad08c37c3
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-20 21:41:18 +0300

    repository/contact add docstrings 2.0

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -47,17 +47,17 @@ The get_contacts function returns a list of contacts from the database.
             else:
                 return db.query(Contact).filter(Contact.first_name == first_name).all()
 
-        if last_name:
-            if email:
-                return db.query(Contact).select_from(Contact) \
-                    .filter(Contact.last_name == last_name, Contact.email == email).all()
-            else:
-                return db.query(Contact).filter(Contact.last_name == last_name).all()
-
+    if last_name:
         if email:
-            return db.query(Contact).filter(Contact.email == email).all()
+            return db.query(Contact).select_from(Contact) \
+                .filter(Contact.last_name == last_name, Contact.email == email).all()
+        else:
+            return db.query(Contact).filter(Contact.last_name == last_name).all()
 
-        return db.query(Contact).offset(skip).limit(limit).all()
+    if email:
+        return db.query(Contact).filter(Contact.email == email).all()
+
+    return db.query(Contact).offset(skip).limit(limit).all()
 
 
 async def verify_email_phone(email: str, phone: str, db: Session):
@@ -72,14 +72,14 @@ The verify_email_phone function is used to verify that the email and phone numbe
 :doc-author: Trelent
 """
     email_data = db.query(Contact.email).filter(Contact.email == email).first()
-        if email_data:
-            return email_data
+    if email_data:
+        return email_data
 
-        phone_data = db.query(Contact.phone).filter(Contact.phone == phone).first()
-        if phone_data:
-            return phone_data
+    phone_data = db.query(Contact.phone).filter(Contact.phone == phone).first()
+    if phone_data:
+        return phone_data
 
-        return None
+    return None
 
 
 async def get_contact_birthday(skip: int,
@@ -96,15 +96,15 @@ The get_contact_birthday function returns a list of contacts with birthdays in t
 :doc-author: Trelent
 """
     contacts_with_next_birth = []
-        today = date.today()
-        all_contacts = db.query(Contact).offset(skip).limit(limit).all()
-        for contact in all_contacts:
-            if contact.date_of_birth.month == today.month:
-                if 0 <= (contact.date_of_birth.day - today.day) <= 7:
-                    contacts_with_next_birth.append(contact)
-            else:
-                continue
-        return contacts_with_next_birth
+    today = date.today()
+    all_contacts = db.query(Contact).offset(skip).limit(limit).all()
+    for contact in all_contacts:
+        if contact.date_of_birth.month == today.month:
+            if 0 <= (contact.date_of_birth.day - today.day) <= 7:
+                contacts_with_next_birth.append(contact)
+        else:
+            continue
+    return contacts_with_next_birth
 
 
 async def create_contact(body: ContactModel, db: Session, user: User):
@@ -118,10 +118,10 @@ The create_contact function creates a new contact in the database.
 :doc-author: Trelent
 """
     contact = Contact(**body.dict(), user_id=user.id)
-        db.add(contact)
-        db.commit()
-        db.refresh(contact)
-        return contact
+    db.add(contact)
+    db.commit()
+    db.refresh(contact)
+    return contact
 
 
 async def update_contact(contact_id: int, body: ContactModel, db: Session, user: User):
@@ -139,14 +139,14 @@ The update_contact function updates a contact in the database.
 :doc-author: Trelent
 """
     contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
-        if contact:
-            contact.first_name = body.first_name
-            contact.last_name = body.last_name
-            contact.email = body.email
-            contact.phone = body.phone
-            contact.date_of_birth = body.date_of_birth
-            db.commit()
-        return contact
+    if contact:
+        contact.first_name = body.first_name
+        contact.last_name = body.last_name
+        contact.email = body.email
+        contact.phone = body.phone
+        contact.date_of_birth = body.date_of_birth
+        db.commit()
+    return contact
 
 
 async def remove_contact(contact_id: int, db: Session, user: User):
@@ -164,7 +164,7 @@ The remove_contact function removes a contact from the database.
 :doc-author: Trelent
 """
     contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
-        if contact:
-            db.delete(contact)
-            db.commit()
-        return contact
+    if contact:
+        db.delete(contact)
+        db.commit()
+    return contact
