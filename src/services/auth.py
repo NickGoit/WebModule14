800b28ed4abd401e539168ed50c573172f0a1779
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-20 21:53:43 +0300

    services/auth add docstrings services/email add docstrings

--- src/services/auth.py
+++ src/services/auth.py
@@ -34,6 +34,17 @@ class Auth:
 
     # define a function to generate a new access token
     async def create_access_token(self, data: dict, expires_delta: Optional[float] = None):
+        """
+    The create_access_token function creates a new access token for the user.
+        The function takes in two arguments: data and expires_delta. Data is a dictionary that contains all of the information about the user, such as their username, email address, etc. Expires_delta is an optional argument that specifies how long you want your access token to be valid for (in minutes). If no value is specified then it defaults to 120 minutes (2 hours).
+        The function first creates a copy of the data dictionary called to_encode and adds three additional key-value pairs: iat which stands for issued at time and represents when
+
+    :param self: Refer to the current object
+    :param data: dict: Pass the data that will be encoded in the token
+    :param expires_delta: Optional[float]: Set the expiration time of the access token
+    :return: A string that is the encoded access token
+    :doc-author: Trelent
+    """
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + timedelta(minutes=expires_delta)
@@ -45,6 +56,18 @@ class Auth:
 
     # define a function to generate a new refresh token
     async def create_refresh_token(self, data: dict, expires_delta: Optional[float] = None):
+        """
+    The create_refresh_token function creates a refresh token for the user.
+        Args:
+            data (dict): A dictionary containing the user's id and username.
+            expires_delta (Optional[float]): The number of seconds until the token expires, defaults to None.
+
+    :param self: Represent the instance of the class
+    :param data: dict: Pass in the user's information
+    :param expires_delta: Optional[float]: Set the expiry time of the token
+    :return: A refresh token that is encoded using the secret_key and algorithm
+    :doc-author: Trelent
+    """
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + timedelta(seconds=expires_delta)
@@ -55,6 +78,17 @@ class Auth:
         return encoded_refresh_token
 
     async def decode_refresh_token(self, refresh_token: str):
+        """
+    The decode_refresh_token function is used to decode the refresh token.
+    It takes a refresh_token as an argument and returns the email of the user if it's valid.
+    If not, it raises an HTTPException with status code 401 (UNAUTHORIZED) and detail 'Could not validate credentials'.
+
+
+    :param self: Represent the instance of a class
+    :param refresh_token: str: Pass the refresh token to the function
+    :return: The email of the user
+    :doc-author: Trelent
+    """
         try:
             payload = jwt.decode(refresh_token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
             if payload['scope'] == 'refresh_token':
@@ -65,6 +99,17 @@ class Auth:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
 
     async def get_current_user(self, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+        """
+    The get_current_user function is a dependency that will be used in the
+        UserResource class. It takes an OAuth2 token as input and returns the user
+        associated with that token. If no user is found, it raises an exception.
+
+    :param self: Represent the instance of the class
+    :param token: str: Get the token from the authorization header
+    :param db: Session: Get the database session
+    :return: A user object, which is used to identify the user
+    :doc-author: Trelent
+    """
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
@@ -97,6 +142,17 @@ class Auth:
         return user
 
     def create_email_token(self, data: dict):
+        """
+    The create_email_token function takes a dictionary of data and returns a token.
+    The token is created using the JWT library, which uses the SECRET_KEY and ALGORITHM to create an encoded string.
+    The data dictionary contains information about the user's email address, as well as when it was issued (iat)
+    and when it expires (exp). The iat and exp values are added to the data dict before encoding.
+
+    :param self: Represent the instance of the class
+    :param data: dict: Pass in the data that we want to encode
+    :return: A token that is encoded with the user's email and a timestamp
+    :doc-author: Trelent
+    """
         to_encode = data.copy()
         expire = datetime.utcnow() + timedelta(days=1)
         to_encode.update({"iat": datetime.utcnow(), "exp": expire})
@@ -104,6 +160,15 @@ class Auth:
         return token
 
     async def get_email_from_token(self, token: str):
+        """
+    The get_email_from_token function takes a token as an argument and returns the email address associated with that token.
+    The function uses the jwt library to decode the token, which is then used to return the email address.
+
+    :param self: Represent the instance of the class
+    :param token: str: Pass in the token that was sent to the user's email
+    :return: The email address associated with the token
+    :doc-author: Trelent
+    """
         try:
             payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
             email = payload["sub"]
--- src/services/email.py
+++ src/services/email.py
@@ -24,6 +24,19 @@ conf = ConnectionConfig(
 
 
 async def send_email(email: EmailStr, username: str, host: str):
+    """
+The send_email function sends an email to the user with a link to confirm their email address.
+    The function takes in three parameters:
+        -email: EmailStr, the user's email address.
+        -username: str, the username of the user who is registering for an account.  This will be used in a greeting message within the body of the email sent to them.
+        -host: str, this is where we are hosting our application (i.e., localhost).  This will be used as part of a URL that they can click on within their browser.
+
+:param email: EmailStr: Specify the email address of the recipient
+:param username: str: Pass the username to the template
+:param host: str: Pass the hostname to the template
+:return: A coroutine which is a special type of object that can be used with await or yield from
+:doc-author: Trelent
+"""
     try:
         token_verification = auth_service.create_email_token({"sub": email})
         message = MessageSchema(
