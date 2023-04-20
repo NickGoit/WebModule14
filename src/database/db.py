481dd76464c169824f38a29f69774760afe52451
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-20 21:48:43 +0300

    routes/auth add docstrings db add docstrings

--- src/database/db.py
+++ src/database/db.py
@@ -14,6 +14,14 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Dependency
 def get_db():
+    """
+The get_db function is a context manager that returns the database session.
+It also handles any exceptions that may occur during the session, and closes
+the connection when it's done.
+
+:return: A database session that can be used to query the database
+:doc-author: Trelent
+"""
     db = SessionLocal()
     try:
         yield db
