2264209937c4a32667f0064c97b89afc62a29020
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-21 19:55:49 +0300

    add documentation

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,19 +2,20 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str
-    secret_key_jwt: str
-    algorithm: str
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
-    redis_host: str
-    redis_port: int
-    cloudinary_name: str
-    cloudinary_api_key: int
-    cloudinary_api_secret: str
+    sqlalchemy_database_url: str = 'postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:' \
+                                   '${POSTGRES_PORT}/${POSTGRES_DB}'
+    secret_key_jwt: str = 'secret'
+    algorithm: str = 'HS256'
+    mail_username: str = 'example@meta.ua'
+    mail_password: str = 'password'
+    mail_from: str = 'example@meta.ua'
+    mail_port: int = '439'
+    mail_server: str = 'smtp.meta'
+    redis_host: str = 'localhost'
+    redis_port: int = 4339
+    cloudinary_name: str = 'temp'
+    cloudinary_api_key: int = 4523469
+    cloudinary_api_secret: str = 'sectret api'
 
     class Config:
         env_file = ".env"
