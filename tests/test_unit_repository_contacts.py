97bb67933c9fbdbb5691f055e04a0044a317829b
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-22 12:07:59 +0300

    add Unittest 1.3

--- tests/test_unit_repository_contacts.py
+++ tests/test_unit_repository_contacts.py
@@ -1,5 +1,7 @@
+import datetime
 import unittest
 from unittest.mock import MagicMock
+from datetime import date
 
 from sqlalchemy.orm import Session
 
@@ -26,7 +28,8 @@ class TestNotes(unittest.IsolatedAsyncioTestCase):
             first_name='Buster',
             last_name='Johns',
             email='buster@meta.ua',
-            phone='+35428421424'
+            phone='+35428421424',
+            date_of_birth=datetime.date(year=1985, month=10, day=8),
         )
 
     async def test_get_contacts(self):
@@ -75,6 +78,23 @@ class TestNotes(unittest.IsolatedAsyncioTestCase):
         self.session.query().filter().first.return_value = contacts
         result = await get_contact_by_id(contact_id=self.contact_test.id, db=self.session)
         self.assertEqual(result, contacts)
+
+    async def test_create_contact(self):
+        body = ContactModel(
+            first_name=self.contact_test.first_name,
+            last_name=self.contact_test.last_name,
+            email=self.contact_test.email,
+            phone=self.contact_test.email,
+            date_of_birth=self.contact_test.date_of_birth,
+        )
+        result = await create_contact(body=body, db=self.session, user=self.user)
+        self.assertEqual(result.first_name, body.first_name)
+        self.assertEqual(result.last_name, body.last_name)
+        self.assertEqual(result.email, body.email)
+        self.assertEqual(result.phone, body.phone)
+        self.assertEqual(result.date_of_birth, body.date_of_birth)
+        self.assertTrue(hasattr(result, "id"))
+
 
 
 if __name__ == '__main__':
