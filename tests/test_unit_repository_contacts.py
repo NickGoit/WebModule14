18c68d8260c36a408c16ea15527b872721c0c2be
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-22 11:18:01 +0300

    add Unittest 1.1

--- tests/test_unit_repository_contacts.py
+++ tests/test_unit_repository_contacts.py
@@ -21,9 +21,20 @@ class TestNotes(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.session = MagicMock(spec=Session)
         self.user = User(id=1)
+        self.contact_test = Contact(
+            id=1,
+            first_name='Buster',
+            last_name='Johns',
+            email='buster@meta.ua',
+            phone='+35428421424'
+        )
 
-    async def test_get_notes(self):
-        notes = [Note(), Note(), Note()]
-        self.session.query().filter().offset().limit().all.return_value = notes
-        result = await get_notes(skip=0, limit=10, user=self.user, db=self.session)
-        self.assertEqual(result, notes)
+    async def test_get_contacts(self):
+        contacts = [self.contact_test, Contact(), Contact()]
+        self.session.query().offset().limit().all.return_value = contacts
+        result = await get_contacts(skip=0, limit=10, first_name='', last_name='', email='', db=self.session)
+        self.assertEqual(result, contacts)
+
+
+if __name__ == '__main__':
+    unittest.main()
