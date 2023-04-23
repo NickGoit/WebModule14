cef419d68eb1831c541ae448106dfbc622c468f8
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-23 21:48:24 +0300

    update Unittest final

--- tests/test_unit_repository_users.py
+++ tests/test_unit_repository_users.py
@@ -1,12 +1,12 @@
 import datetime
 import unittest
 from unittest.mock import MagicMock
-from datetime import date
+
 
 from sqlalchemy.orm import Session
 
 from src.database.models import Contact, User
-from src.schemas import ContactModel, UserModel
+from src.schemas import UserModel
 from src.repository.users import (
     get_user_by_email,
     create_user,
@@ -24,7 +24,9 @@ class TestUsers(unittest.IsolatedAsyncioTestCase):
             id=1,
             username='User1',
             email='user1@gmail.com',
-            password ='qwerty')
+            password='qwerty',
+            confirmed=True,
+        )
         self.contact_test = Contact(
             id=1,
             first_name='Buster',
@@ -35,23 +37,39 @@ class TestUsers(unittest.IsolatedAsyncioTestCase):
         )
 
     async def test_get_user_by_email(self):
-        contact = self.contact_test
-        self.session.query().filter().first.return_value = contact
+        user = self.user
+        self.session.query().filter().first.return_value = user
         result = await get_user_by_email(email=self.user.email, db=self.session)
-        self.assertEqual(result, contact)
+        self.assertEqual(result, user)
 
     async def test_create_user(self):
         body = UserModel(
-                        username=self.user.username,
-                        email=self.user.email,
-                        password=self.user.password,
-            )
+            username=self.user.username,
+            email=self.user.email,
+            password=self.user.password,
+        )
         result = await create_user(body=body, db=self.session)
 
         self.assertEqual(result.username, body.username)
         self.assertEqual(result.email, body.email)
         self.assertEqual(result.password, body.password)
         self.assertTrue(hasattr(result, "id"))
+
+    async def test_confirmed_email(self):
+        result = await confirmed_email(email=self.user.email, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_update_token(self):
+        user = self.user
+        token = None
+        result = await update_token(user=user, token=token, db=self.session)
+        self.assertIsNone(result)
+
+    async def update_avatar(self):
+        url = 'https://res.cloudinary.com/web9storage/image/upload/c_fill,h_250,w_250/v1/Web9_FastapiAPP/Johny3'
+        user = self.user
+        result = await update_avatar(email=self.user.email, url=url, db=self.session)
+        self.assertEqual(result, user)
 
 
 
